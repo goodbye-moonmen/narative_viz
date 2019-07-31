cc21773557355eb149254285139daab2294e2f0c
goodbye-moonmen <hangzhu2@illinois.edu>
2019-07-30 23:31:33 -0400

    update

--- interactivePie.js
+++ interactivePie.js
@@ -1,48 +1,83 @@
-async function visualize(){
-    const height = 500;
-    const width = 500;
-    const selectedDecate = 2010;
-    const radius = Math.min(height,width) / 2;
-    // const color = d3.scale.category20();
-    // const genreColors = (i)=>d3.interpolateViridis(d3.scaleOrdinal().domain(GENRES_CONST)(i));
-    const data = await loadData();
-    const decadeData = data.filter((d)=>(d.year === selectedDecate && !!d.genres));
-    const computedRevenuData = computeRevenueData(decadeData);
-    const pie = d3.pie().value((d)=>{
-        console.log(d);
-        return d.revenue;
-    }).sort(null);
-    const svg = d3.select("#donut")
-                .append("svg")
-                .attr("width",width)
-                .attr("height",height)
-                .append("g")
-                .attr("transform",`translate(${width / 2}, ${height / 2})`);
-    const arc = d3.arc().innerRadius(radius - 100).outerRadius(radius - 20);
-    svg.selectAll("path").data(pie(computedRevenuData)).enter().append("path")
-    .attr("fill",(d,i)=>{return d3.interpolateSinebow(i/GENRES_CONST.length);})
-    .attr("d",arc)
-    .attr("stroke","grey")
-    .attr("stroke-width","2");
+let currentDecade = 1950;
+let previousDecade = 1950;
+const height = 500;
+const width = 500;
+let globalData;
+
+function setDecade(decade) {
+	previousDecade = currentDecade;
+	currentDecade = decade;
+	document.getElementById(`decade${previousDecade}`).classList.remove("selectedDecade");
+	document.getElementById(`decade${currentDecade}`).classList.add("selectedDecade");
+	clearExistingPieChart();
+	drawPieChartByDecade(currentDecade, globalData, height, width);
 }
 
 async function loadData() {
-    const isLocal = true;
-    const url = (isLocal ? "https://goodbye-moonmen.github.io/narative_viz/" : "") + "movieSimple.tsv";
-    const data = await d3.tsv(url,transformData);
-    return data;
+	d3.selectAll(".decadeButton").attr("disabled", true);
+	globalData = await fetchDataFromWeb();
+	d3.selectAll(".decadeButton").attr("disabled", false);
 }
 
-function computeRevenueData(decadeData){
-    const result = GENRES_CONST.map((g)=>({name:g,revenue:0}));
-    decadeData.forEach((d)=>{
-        const splitRevenue = d.revenue/d.genres.length;
-        d.genres.forEach((g)=>{
-            const index = GENRES_CONST.indexOf(g);
-            result[index]["revenue"] = result[index]["revenue"] + splitRevenue;
-        });
-    });
-    return result;
+async function fetchDataFromWeb() {
+	const isLocal = true;
+	const url = (isLocal ? "https://goodbye-moonmen.github.io/narative_viz/" : "") + "movieSimple.tsv";
+	const data = await d3.tsv(url, transformData);
+	return data;
+}
+
+function drawPieChartByDecade(selectedDecade, data, height, width) {
+	const radius = Math.min(height, width) / 2;
+	const decadeData = data.filter((d) => d.year === selectedDecade && !!d.genres);
+	const computedRevenuData = computeRevenueData(decadeData);
+	const pie = d3
+		.pie()
+		.value((d) => d.revenue)
+		.sort(null);
+	const svg = d3
+		.select("#donut")
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.append("g")
+		.attr("transform", `translate(${width / 2}, ${height / 2})`);
+	const arc = d3
+		.arc()
+		.innerRadius(radius - 100)
+		.outerRadius(radius - 20);
+	svg.selectAll("path")
+		.data(pie(computedRevenuData))
+		.enter()
+		.append("path")
+		.attr("fill", (_d, i) => d3.interpolateSinebow(i / GENRES_CONST.length))
+		.attr("d", arc)
+		.attr("stroke", "#eeecea")
+		.attr("stroke-width", "2")
+		.on("mouseover", (d, i) => handlePiePieceHover(d, i));
+	svg.append("div")
+		.attr("width", width)
+		.attr("height", height)
+		.attr("transform", `translate(${width / 2}, ${height / 2})`);
+}
+
+function clearExistingPieChart() {
+	d3.select("#donut").html("");
+}
+
+function handlePiePieceHover(d, i) {
+	currentHighlightGenre = GENRES_CONST[i];
+}
+
+function computeRevenueData(decadeData) {
+	const result = GENRES_CONST.map((g) => ({ name: g, revenue: 0 }));
+	decadeData.forEach((d) => {
+		const splitRevenue = d.revenue / d.genres.length;
+		d.genres.forEach((g) => {
+			const index = GENRES_CONST.indexOf(g);
+			result[index]["revenue"] = result[index]["revenue"] + splitRevenue;
+		});
+	});
+	return result;
 }
 
 // Data keys:
@@ -51,45 +86,45 @@ function computeRevenueData(decadeData){
 // Title
 // Genres
 // Poster Path
-// Number of Records 
+// Number of Records
 // Revenue
-function transformData(entry){
-    const processedGenres = processGenres(entry["Genres"]);
-    if(!processGenres){
-        console.warn("Invalid entry:"+entry["Genres"]);
-        return null;
-    }else{
-        return ({
-            title: entry["Title"],
-            decade: Number(entry["Decade"]),
-            year: Number(entry["Year of Release Date"]),
-            genres: processedGenres,
-            revenue: Number(entry["Revenue"].replace(/,/g,""))/1000000,
-        });
-    }
+function transformData(entry) {
+	const processedGenres = processGenres(entry["Genres"]);
+	if (!processGenres) {
+		console.warn("Invalid entry:" + entry["Genres"]);
+		return null;
+	} else {
+		return {
+			title: entry["Title"],
+			decade: Number(entry["Decade"]),
+			year: Number(entry["Year of Release Date"]),
+			genres: processedGenres,
+			revenue: Number(entry["Revenue"].replace(/,/g, "")) / 1000000,
+		};
+	}
 }
 
 const GENRES_CONST = [
-    "Action",
-    "Adventure",
-    "Animation",
-    "Comedy",
-    "Crime",
-    "Documentary",
-    "Drama",
-    "Family",
-    "Fantasy",
-    "Foreign",
-    "History",
-    "Horror",
-    "Music",
-    "Mystery",
-    "Romance",
-    "Science Fiction",
-    "TV Movie",
-    "Thriller",
-    "War",
-    "Western",
+	"Action",
+	"Adventure",
+	"Animation",
+	"Comedy",
+	"Crime",
+	"Documentary",
+	"Drama",
+	"Family",
+	"Fantasy",
+	"Foreign",
+	"History",
+	"Horror",
+	"Music",
+	"Mystery",
+	"Romance",
+	"Science Fiction",
+	"TV Movie",
+	"Thriller",
+	"War",
+	"Western",
 ];
 // All possible genres
 // Action: 1716
@@ -114,17 +149,17 @@ const GENRES_CONST = [
 // Western: 109
 
 // Genre keys:
-// id 
+// id
 // name
-function processGenres(rawGenres){
-    try{
-        const entryGenres = JSON.parse(rawGenres.replace(/\'/g,"\""));
-        const genresArray = entryGenres.map((genreObj)=>genreObj.name);
-        if(genresArray.includes("TV Movies")){
-            return undefined;
-        }
-        return genresArray;
-    }catch(e){
-        return undefined;
-    }
+function processGenres(rawGenres) {
+	try {
+		const entryGenres = JSON.parse(rawGenres.replace(/\'/g, '"'));
+		const genresArray = entryGenres.map((genreObj) => genreObj.name);
+		if (genresArray.includes("TV Movies")) {
+			return undefined;
+		}
+		return genresArray;
+	} catch (e) {
+		return undefined;
+	}
 }
